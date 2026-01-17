5ec459fc69b8f640bff5a5d1527374a4eb02fc88
Dvd-Znf <email@dvdznf.xyz>
2026-01-17 13:35:15 +0100

    new package fromGithub: Cloudflare.cloudflared

---
+++ winget-pkgs/fromGithub/Cloudflare.cloudflared.nix
@@ -0,0 +1,9 @@
+{
+  repo = "cloudflare/cloudflared";
+  targets = [
+    "cloudflared-windows-amd64.msi"
+    "cloudflared-windows-386.msi"
+    "cloudflared-windows-amd64.exe"
+    "cloudflared-windows-386.exe"
+  ];
+}

90e072f8f68848658c39dd60ed2640715a03c19a
Dvd-Znf <email@dvdznf.xyz>
2026-01-17 22:47:10 +0100

    komac: Use 0554f01 patch

--- flake.nix
+++ flake.nix
@@ -24,7 +24,19 @@
         runtimeInputs = with pkgs; [
           curl
           jq
-          komac
+          (komac.overrideAttrs (rec {
+            src = fetchFromGitHub {
+              owner = "Dvd-Znf";
+              repo = "Komac";
+              rev = "0554f01e566cb4327358cbe366bf0cfd8b62ccf0";
+              hash = "sha256-Woh2NNCxQkiJH/9BJpykC3Fy+nf6Oh4Zgs3If4/pt8g=";
+            };
+            cargoDeps = rustPlatform.fetchCargoVendor {
+              inherit src;
+              hash = "sha256-YloeJgP4wDQ4JHn+Rw8pSm5Dsx1sdz/s1CLMj5LOZ5s=";
+            };
+          }))
+
         ];
         text = builtins.concatStringsSep "\n" (builtins.map (lib.updateFromGithub) gh-manifests);
         excludeShellChecks = [ "SC2086" ];

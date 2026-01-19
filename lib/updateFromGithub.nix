b33f8483ce029834217b28dfc60c86bd2845c18b
Dvd-Znf <email@dvdznf.xyz>
2026-01-19 18:38:45 +0100

    fix spellcheck: SC1101 SC2046

--- lib/updateFromGithub.nix
+++ lib/updateFromGithub.nix
@@ -8,10 +8,10 @@
   tag_name=$(curl -s https://api.github.com/repos/${repo}/releases/latest | jq -r '.tag_name')
   download_url=https://github.com/${repo}/releases/download/"$tag_name"
 
-  komac update ${id} \ 
+  komac update ${id} \
   --submit --urls \
   ${builtins.concatStringsSep "\n" (map (target: "\"$download_url/\"" + target + " \\") targets)}
   --version "''${tag_name#v}" \
-  ${if autoreplace then "--replace $(komac list-versions ${id} | head -n 1)" else ""} \
+  ${if autoreplace then "--replace \"$(komac list-versions ${id} | head -n 1)\"" else ""} \
   & disown;
 ''

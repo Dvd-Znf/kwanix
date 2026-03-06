607703c04a1b51b86000916eaa77aef9d3265add
Dvd-Znf <email@dvdznf.xyz>
2026-03-06 21:58:05 +0100

    feat: disabled attribute to updates

--- lib/updateFromGithub.nix
+++ lib/updateFromGithub.nix
@@ -3,15 +3,19 @@
   repo,
   targets,
   autoreplace ? false,
+  disabled ? false,
 }:
-''
-  tag_name=$(curl -s https://api.github.com/repos/${repo}/releases/latest | jq -r '.tag_name')
-  download_url=https://github.com/${repo}/releases/download/"$tag_name"
+if disabled then
+  ""
+else
+  ''
+    tag_name=$(curl -s https://api.github.com/repos/${repo}/releases/latest | jq -r '.tag_name')
+    download_url=https://github.com/${repo}/releases/download/"$tag_name"
 
-  komac update ${id} \
-  --submit --urls \
-  ${builtins.concatStringsSep "\n" (map (target: "\"$download_url/\"" + target + " \\") targets)}
-  --version "''${tag_name#v}" \
-  ${if autoreplace then "--replace \"$(komac list-versions ${id} | head -n 1)\"" else ""} \
-  & disown;
-''
+    komac update ${id} \
+    --submit --urls \
+    ${builtins.concatStringsSep "\n" (map (target: "\"$download_url/\"" + target + " \\") targets)}
+    --version "''${tag_name#v}" \
+    ${if autoreplace then "--replace \"$(komac list-versions ${id} | head -n 1)\"" else ""} \
+    & disown;
+  ''
--- lib/updateFromScraped.nix
+++ lib/updateFromScraped.nix
@@ -2,13 +2,17 @@
   id,
   fetcher,
   autoreplace ? false,
+  disabled ? false,
 }:
-fetcher
-+ ''
-   komac update ${id} \
-  --submit --urls \
-  ''${urls[*]} \
-  --version "''${version#v}" \
-  ${if autoreplace then "--replace \"$(komac list-versions ${id} | head -n 1)\"" else ""} \
-  & disown;
-''
+if disabled then
+  ""
+else
+  fetcher
+  + ''
+     komac update ${id} \
+    --submit --urls \
+    ''${urls[*]} \
+    --version "''${version#v}" \
+    ${if autoreplace then "--replace \"$(komac list-versions ${id} | head -n 1)\"" else ""} \
+    & disown;
+  ''

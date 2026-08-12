6a03fc0597f94bf28b5054d8efc9d98ed771db44
Dvd-Znf <email@dvdznf.xyz>
2026-08-12 22:38:31 +0300

    fromGithub luolangaga.tubatools: added missing installers

--- winget-pkgs/fromGithub/luolangaga.tubatools.nix
+++ winget-pkgs/fromGithub/luolangaga.tubatools.nix
@@ -1,6 +1,8 @@
 {
   repo = "luolangaga/tubatools";
   targets = [
+    "TubaWinUi3_Setup_\${tag_name:1}_x86.exe"
     "TubaWinUi3_Setup_\${tag_name:1}_x64.exe"
+    "TubaWinUi3_Setup_\${tag_name:1}_arm64.exe"
   ];
 }

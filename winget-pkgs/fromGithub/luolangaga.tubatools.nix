6a4afc831342ef456ab40dba986f164c55b4bade
Dvd-Znf <email@dvdznf.xyz>
2026-09-04 23:20:24 +0200

    fromGithub luolangaga.tubatools: drop x86

--- winget-pkgs/fromGithub/luolangaga.tubatools.nix
+++ winget-pkgs/fromGithub/luolangaga.tubatools.nix
@@ -1,7 +1,6 @@
 {
   repo = "luolangaga/tubatools";
   targets = [
-    "TubaWinUi3_Setup_\${tag_name:1}_x86.exe"
     "TubaWinUi3_Setup_\${tag_name:1}_x64.exe"
     "TubaWinUi3_Setup_\${tag_name:1}_arm64.exe"
   ];
